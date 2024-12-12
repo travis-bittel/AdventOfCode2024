e93d860c7db8b4f4e8bbd066e62ab92cf728b99f
Travis Bittel (tbittel) <tbittel@cisco.com>
2024-12-12 13:26:40 -0500

    Clean up Day 12

--- day_12.py
+++ day_12.py
@@ -1,63 +1,44 @@
 import time
+from dataclasses import dataclass
 
 from util.grid_utils import Direction, Grid, Vector2D
 
 
-def part_1(grid: Grid):
+@dataclass
+class Region:
+    area: int
+    perimeter: int
+    all_cells: set[Vector2D]
+    perimeter_cells: set[(Vector2D, Direction)]
+    plant_type: str
+
+
+def get_regions(grid: Grid) -> [Region]:
+    regions = []
     unvisited_set = set()
     unvisited_set.update(grid.all_positions())
-
-    total_price = 0
     while unvisited_set:
         start = next(iter(unvisited_set))
         to_explore = [start]
+        all_cells = set()
+        perimeter_cells = set()
         plant_type = grid.get(*start)
-        region_area = 0
-        region_perimeter = 0
         while to_explore:
             current_position = to_explore.pop()
             if current_position in unvisited_set:
+                all_cells.add(current_position)
                 unvisited_set.remove(current_position)
             else:
                 continue
             adjacent_cells_of_same_type \
                 = [adjacent_position for adjacent_position in grid.adjacent_positions(*current_position)
                    if grid.get(*adjacent_position) == plant_type]
-            region_perimeter += 4 - len(adjacent_cells_of_same_type)
-            region_area += 1
-            to_explore.extend(adjacent_cells_of_same_type)
-        total_price += region_area * region_perimeter
-    print(total_price)
-
-
-def part_2(grid: Grid):
-    unvisited_set = set()
-    unvisited_set.update(grid.all_positions())
-
-    total_price = 0
-    while unvisited_set:
-        start = next(iter(unvisited_set))
-        to_explore = [start]
-        plant_type = grid.get(*start)
-        region_area = 0
-        perimeter_cells = set()
-        while to_explore:
-            current_position = to_explore.pop()
-            if current_position in unvisited_set:
-                unvisited_set.remove(current_position)
-            else:
-                continue
-            adjacent_cells_of_same_type = [adjacent_position for adjacent_position
-                                           in grid.adjacent_positions(*current_position)
-                                           if grid.get(*adjacent_position) == plant_type]
             for direction in list(Direction):
                 if current_position + direction.value not in adjacent_cells_of_same_type:
                     perimeter_cells.add((current_position, direction))
-            region_area += 1
             to_explore.extend(adjacent_cells_of_same_type)
-        sides = number_of_sides(perimeter_cells, grid)
-        total_price += region_area * sides
-    print(total_price)
+        regions.append(Region(len(all_cells), len(perimeter_cells), all_cells, perimeter_cells, plant_type))
+    return regions
 
 
 def number_of_sides(perimeter_cells: set[(Vector2D, Direction)], grid: Grid) -> int:
@@ -91,6 +72,16 @@ def number_of_sides(perimeter_cells: set[(Vector2D, Direction)], grid: Grid) -> 
                     if Vector2D.is_vertically_adjacent(current_position, perimeter_cell[0]):
                         to_explore.add(perimeter_cell)
     return sides
+
+
+def part_1(grid: Grid):
+    regions = get_regions(grid)
+    print(sum(region.area * region.perimeter for region in regions))
+
+
+def part_2(grid: Grid):
+    regions = get_regions(grid)
+    print(sum(region.area * number_of_sides(region.perimeter_cells, grid) for region in regions))
 
 
 if __name__ == '__main__':
